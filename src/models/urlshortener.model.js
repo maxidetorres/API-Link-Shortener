69a09a5b7f192014254c4f36d6879b9783d759c2
maxidetorres <maxidetorres@gmail.com>
2020-06-16 11:09:49 -0300

    merge rama-bucha

--- src/models/urlshortener.model.js
+++ src/models/urlshortener.model.js
@@ -1,11 +1,11 @@
-import { Schema as _Schema, model } from 'mongoose'; 
-const {Schema} = _Schema;
+import { Schema as _Schema, model } from 'mongoose'
+const { Schema } = _Schema
 
 const UrlSchema = new Schema({
-    originalUrl: {type: String,required: true},
-    urlCode: {type: String,required: true},
-    shortUrl: {type: String,required: true}
+  originalUrl: { type: String, required: true },
+  urlCode: { type: String, required: true },
+  shortUrl: { type: String, required: true }
   //  expireAt:{type: Date, expires: 1200, default: Date.now}
-});
+})
 
-export default model('Url', UrlSchema);
+export default model('Url', UrlSchema)
