e1496ac24d043e23d80840d98181319b60bd2289
Emilio Buchaillot <emiliobuchaillot@gmail.com>
2020-06-16 10:45:24 -0300

    Agregado npm run lint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-//const routes = require('./routes')
+// const routes = require('./routes')
 
 const app = express()
 
@@ -7,15 +7,15 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
   next()
-});
+})
 
 app.use(express.json())
 
-//app.use('/', routes)
+// app.use('/', routes)
 
 const port = process.env.PORT || 3000
-  app.listen(port, () => {
-   console.log(`El servidor está inicializado en el puerto ${port}`);
-  })
+app.listen(port, () => {
+  console.log(`El servidor está inicializado en el puerto ${port}`)
+})
 
 module.exports = app
